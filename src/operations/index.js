d8dfd17b6601202d76defbfb2feceac7ac2112bc
a.kashina <a.kashina@websecret.by>
2023-06-26 16:03:06 +0200

    feat: hash operation

--- src/operations/index.js
+++ src/operations/index.js
@@ -1,10 +1,12 @@
 import cd from "./cd.js";
+import hash from "./hash.js";
 import ls from "./ls.js";
 import os from "./os.js";
 import up from "./up.js";
 
 const operations = {
     cd,
+    hash,
     ls,
     os,
     up,
